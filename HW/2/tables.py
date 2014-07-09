971bf9125072ba9b1a6b408384b9daa6ca5e963d
Yamez <jcook7104@sohamcollege.org.uk>
2014-07-09 13:33:08 +0100

    my times table
    my version of the times table program

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,14 @@
-#paste code here
+x = int(input("Enter A Number"))
+print(x*1)
+print(x*2)
+print(x*3)
+print(x*4)
+print(x*5)
+print(x*6)
+print(x*7)
+print(x*8)
+print(x*9)
+print(x*10)
+print(x*11)
+print(x*12)
+
